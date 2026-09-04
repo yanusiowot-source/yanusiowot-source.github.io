e2cc873d9789a7b9b64adfffdce5b9e1eafc7cd1
Ян Ярошевич <yan@genesisengine.app>
2026-09-04 14:43:07 +0000

    Public lab: order a copy via X message

---
+++ genesis-lab/assets/__vite-browser-external-D6XQowSE.js
@@ -0,0 +1,1 @@
+import{n as e}from"./index-DbdXR57s.js";var t=e(((e,t)=>{t.exports={}}));export default t();
--- genesis-lab/assets/__vite-browser-external-WoWKqk6g.js
+++
@@ -1,1 +0,0 @@
-import{n as e}from"./index-CbEv58XC.js";var t=e(((e,t)=>{t.exports={}}));export default t();
