5f919c1f085c02af2ffd33bbc72a049fcfccd284
Ян Ярошевич <yan@genesisengine.app>
2026-09-04 13:51:48 +0000

    Public lab: birth flash and creation line

---
+++ genesis-lab/assets/__vite-browser-external-BkD57WJq.js
@@ -0,0 +1,1 @@
+import{n as e}from"./index-BPv2q_n_.js";var t=e(((e,t)=>{t.exports={}}));export default t();
--- genesis-lab/assets/__vite-browser-external-DsB3-9-q.js
+++
@@ -1,1 +0,0 @@
-import{n as e}from"./index-BN_ZFGqv.js";var t=e(((e,t)=>{t.exports={}}));export default t();
