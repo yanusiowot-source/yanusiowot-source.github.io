d78093c03bd458bfb028d4bcb3c688e91e01f227
Ян Ярошевич <yan@genesisengine.app>
2026-09-04 15:16:06 +0000

    Public lab: shorter first-run, birth is the last step

--- genesis-lab/assets/__vite-browser-external-D-v9cwwe.js
+++
@@ -1,1 +0,0 @@
-import{n as e}from"./index-CM-0QxFG.js";var t=e(((e,t)=>{t.exports={}}));export default t();
---
+++ genesis-lab/assets/__vite-browser-external-DwvLX6HF.js
@@ -0,0 +1,1 @@
+import{n as e}from"./index-B_g4KRjJ.js";var t=e(((e,t)=>{t.exports={}}));export default t();
