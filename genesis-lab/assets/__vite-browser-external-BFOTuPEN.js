b521f84b21451023cc56318bf01e60262787bb38
Ян Ярошевич <yan@genesisengine.app>
2026-09-05 21:52:52 +0000

    Laws fit and Desk walk as you enter

---
+++ genesis-lab/assets/__vite-browser-external-BFOTuPEN.js
@@ -0,0 +1,1 @@
+import{n as e}from"./index-DWZ0kqjP.js";var t=e(((e,t)=>{t.exports={}}));export default t();
--- genesis-lab/assets/__vite-browser-external-Db89N-0y.js
+++
@@ -1,1 +0,0 @@
-import{n as e}from"./index-CVqqfEl2.js";var t=e(((e,t)=>{t.exports={}}));export default t();
