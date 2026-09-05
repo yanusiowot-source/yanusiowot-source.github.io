88be4c97851e1b23885b08aa47f4e038918978ca
Ян Ярошевич <yan@genesisengine.app>
2026-09-05 21:58:40 +0000

    Desk reads the posterior: intervals, R-hat, corridor mass

--- genesis-lab/assets/__vite-browser-external-BFOTuPEN.js
+++
@@ -1,1 +0,0 @@
-import{n as e}from"./index-DWZ0kqjP.js";var t=e(((e,t)=>{t.exports={}}));export default t();
---
+++ genesis-lab/assets/__vite-browser-external-CCCj8Pwa.js
@@ -0,0 +1,1 @@
+import{n as e}from"./index-CUYY_Es2.js";var t=e(((e,t)=>{t.exports={}}));export default t();
