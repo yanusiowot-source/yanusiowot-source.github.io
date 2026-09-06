698497509c54339857ba6582a6ce972a47b5a235
Ян Ярошевич <271322481+yanusiowot-source@users.noreply.github.com>
2026-09-06 00:12:51 +0000

    A still disk does not spend a frame

--- genesis-lab/assets/__vite-browser-external-D_kwLYvF.js
+++
@@ -1,1 +0,0 @@
-import{n as e}from"./index-D-m-LMeW.js";var t=e(((e,t)=>{t.exports={}}));export default t();
---
+++ genesis-lab/assets/__vite-browser-external-QhCs1atf.js
@@ -0,0 +1,1 @@
+import{t as e}from"./rolldown-runtime-W7wSyTde.js";var t=e(((e,t)=>{t.exports={}}));export default t();
