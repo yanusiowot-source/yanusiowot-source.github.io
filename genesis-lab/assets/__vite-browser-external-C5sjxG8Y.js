59f4374a1acb3fd67457c08e81acb20b580557e1
Ян Ярошевич <271322481+yanusiowot-source@users.noreply.github.com>
2026-09-06 00:03:33 +0000

    A short sky still has a birth

--- genesis-lab/assets/__vite-browser-external-C5sjxG8Y.js
+++
@@ -1,1 +0,0 @@
-import{n as e}from"./index-DNut7Cx4.js";var t=e(((e,t)=>{t.exports={}}));export default t();
---
+++ genesis-lab/assets/__vite-browser-external-D_kwLYvF.js
@@ -0,0 +1,1 @@
+import{n as e}from"./index-D-m-LMeW.js";var t=e(((e,t)=>{t.exports={}}));export default t();
